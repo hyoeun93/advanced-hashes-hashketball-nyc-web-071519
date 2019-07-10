c781ee99cbc88119087942e38777687984a06b22
HyoEun Lee <hyoeun93@gmail.com>
2019-07-10 01:05:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,8 +119,7 @@ end
 def num_points_scored(player_name)
   game_hash.values.each do |team|  
     team[:players].each do |player|
-      if player.has_value?(player_name)
-      return player[:points] 
+      return player[:points] if player.has_value?(player_name)
     end
   end
 end 
