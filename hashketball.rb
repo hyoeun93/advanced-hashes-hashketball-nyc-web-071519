f738ec3dcc84c76903371a7caee1351ae842dd81
HyoEun Lee <hyoeun93@gmail.com>
2019-07-10 01:46:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -264,7 +264,8 @@ def long_name_steals_a_ton?
       steals_most, most_steals = player[:player_name], player[:steals] if player[:steals] > most_steals
     end
   end
-  return true if steals_most == player_with_longest_name
+  if steals_most == player_with_longest_name
+    return true 
  end
 
 
